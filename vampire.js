e02d43f1a36b8100ebbd26210d99150dfca7d1d6
Sam <samjulian6@gmail.com>
2022-10-07 01:43:41 -0700

    Implemented methods for Vampire class so that unit tests pass

--- vampire.js
+++ vampire.js
@@ -10,22 +10,29 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let numVampires = 0;
+    let currentVampire = this;
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator
+      numVampires++;
+    }
+    return numVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal);
   }
 
   /** Stretch **/
@@ -36,9 +43,22 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
-
+    let vampire1 = this;
+    let vampire2 = vampire;
+    while (vampire1 !== vampire2) {
+      if (vampire1.numberOfVampiresFromOriginal > vampire2.numberOfVampiresFromOriginal) {
+        vampire1 = vampire1.creator;
+        continue;
+      }
+      if (vampire1.numberOfVampiresFromOriginal < vampire2.numberOfVampiresFromOriginal) {
+        vampire2 = vampire2.creator;
+        continue;
+      }
+      vampire1 = vampire1.creator;
+      vampire2 = vampire2.creator;
+    }
+    return vampire1;
   }
 }
 
 module.exports = Vampire;
-
